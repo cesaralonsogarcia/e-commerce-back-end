5154993a663b02cdce2960beb6a9a47dffc3d1b4
Cesar Alonso Garcia <cesarg324@gmail.com>
2023-05-19 19:04:35 -0500

    Fix problem with category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,9 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      attributes: {
         include: [{ model: Product }]
-      }
     });
     res.status(200).json(categoryData);
   } catch (err) {
